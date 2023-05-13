82d3b276c2033081713359a19f2de0758fdf0420
Nikhil Menghani <nikhil@menghani.com>
2023-05-12 20:46:16 -0400

    Add update_version.py

---
+++ update_version.py
@@ -0,0 +1,19 @@
+import sys
+
+version = sys.argv[1]
+version_line_index = 5
+
+with open('setup.py', 'r') as file:
+    lines = file.readlines()
+
+for i, line in enumerate(lines):
+    if line.strip().startswith('version'):
+        version_line_index = i
+        break
+
+new_version_line = f'version="{version}",\n'
+
+lines[version_line_index] = new_version_line
+
+with open('setup.py', 'w') as file:
+    file.writelines(lines)
