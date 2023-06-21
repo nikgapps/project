7ed92c6086ccd4bffd1d7df18197d0e7b511d8bd
Nikhil Menghani <nikhil@menghani.com>
2023-06-20 20:05:48 -0400

    Fix the appset defaulting to 1 and update config objects

--- NikGapps/helper/NikGappsConfig.py
+++ NikGapps/helper/NikGappsConfig.py
@@ -51,31 +51,39 @@ class NikGappsConfig:
         self.arch = arch
         self.creator = "Nikhil Menghani"
 
-    def build_config_objects(self):
-        android_version = ConfigObj("AndroidVersion", self.android_version)
-        version = ConfigObj("Version", self.config_version)
-        log_directory = ConfigObj("LogDirectory", self.default_mode)
+    def build_config_objects(self, config_dict=None):
+        android_version = ConfigObj("AndroidVersion",
+                                    self.android_version if config_dict is None else config_dict["AndroidVersion"])
+        version = ConfigObj("Version", self.config_version if config_dict is None else config_dict["Version"])
+        log_directory = ConfigObj("LogDirectory",
+                                  self.default_mode if config_dict is None else config_dict["LogDirectory"])
         log_directory.description = """# set this to the directory you want to copy the logs to.
 # for e.g. LogDirectory="/system/etc" will install the logs to /system/etc/nikgapps_logs directory
 # by default it will install it to /sdcard/NikGapps/nikgapps_logs"""
-        install_partition = ConfigObj("InstallPartition", self.default_mode)
+        install_partition = ConfigObj("InstallPartition",
+                                      self.default_mode if config_dict is None else config_dict["InstallPartition"])
         install_partition.description = "# set to /system, /product or" \
                                         " /system_ext if you want to force the installation to aforementioned locations"
-        mode = ConfigObj("mode", self.install_mode)
+        mode = ConfigObj("mode", self.install_mode if config_dict is None else config_dict["mode"])
         mode.description = "# set to uninstall if you want to uninstall any google app, " \
                            "also set the value of google app below to -1"
-        wipe_dalvik_cache = ConfigObj("WipeDalvikCache", self.enabled_mode)
+        wipe_dalvik_cache = ConfigObj("WipeDalvikCache",
+                                      self.enabled_mode if config_dict is None else config_dict["WipeDalvikCache"])
         wipe_dalvik_cache.description = "# set WipeDalvikCache=0 if you don't want the installer " \
                                         "to wipe dalvik/cache after installing the gapps"
-        wipe_runtime_permissions = ConfigObj("WipeRuntimePermissions", self.disabled_mode)
+        wipe_runtime_permissions = ConfigObj("WipeRuntimePermissions",
+                                             self.disabled_mode if config_dict is None else config_dict[
+                                                 "WipeRuntimePermissions"])
         wipe_runtime_permissions.description = "# set WipeRuntimePermissions=1 if you want to wipe runtime permissions"
-        execute_d = ConfigObj("execute.d", self.enabled_mode)
+        execute_d = ConfigObj("execute.d", self.enabled_mode if config_dict is None else config_dict["execute.d"])
         execute_d.description = "# Addon.d config set it to 0 to skip the " \
                                 "automatic backup/restore while flashing the rom"
-        use_zip_config = ConfigObj("use_zip_config", self.use_zip_config)
+        use_zip_config = ConfigObj("use_zip_config",
+                                   self.use_zip_config if config_dict is None else config_dict["use_zip_config"])
         use_zip_config.description = "# if you want to force the installer to use the config from gapps zip file, " \
                                      "set below to 1"
-        gms_optimization = ConfigObj("gms_optimization", self.disabled_mode)
+        gms_optimization = ConfigObj("gms_optimization",
+                                     self.disabled_mode if config_dict is None else config_dict["gms_optimization"])
         gms_optimization.description = "# set this to 1 if you want to enable gms optimization, " \
                                        "careful while doing it, you may experience issues like delayed notification " \
                                        "with some Roms"
@@ -178,9 +186,10 @@ class NikGappsConfig:
                         entry = ">>" + pkg.package_title
                         nikgapps_config_lines += entry + "=" + self.get_dictionary_value(entry) + "\n"
                 else:
-                    nikgapps_config_lines += app_set.title + "=" + str(1) + "\n"
+                    nikgapps_config_lines += app_set.title + "=" + ("0" if override else str(1)) + "\n"
                     for pkg in app_set.package_list:
-                        nikgapps_config_lines += ">>" + pkg.package_title + "=" + ("0" if override else str(pkg.enabled)) + "\n"
+                        nikgapps_config_lines += ">>" + pkg.package_title + "=" + (
+                            "0" if override else str(pkg.enabled)) + "\n"
                 nikgapps_config_lines += "\n"
             else:
                 if config_dict is not None:
@@ -189,12 +198,13 @@ class NikGappsConfig:
                         nikgapps_config_lines += entry + "=" + self.get_dictionary_value(entry) + "\n"
                 else:
                     for pkg in app_set.package_list:
-                        nikgapps_config_lines += pkg.package_title + "=" + ("0" if override else str(pkg.enabled)) + "\n"
+                        nikgapps_config_lines += pkg.package_title + "=" + (
+                            "0" if override else str(pkg.enabled)) + "\n"
         for app_set in NikGappsPackages.get_packages("go", self.android_version):
             if len(app_set.package_list) > 1:
                 nikgapps_config_lines += "\n\n# Set " + app_set.title + "=0 if you want to skip installing all " \
-                                                                    "packages belonging to " \
-                                                                    "" + app_set.title + " Package\n"
+                                                                        "packages belonging to " \
+                                                                        "" + app_set.title + " Package\n"
                 if config_dict is not None:
                     nikgapps_config_lines += app_set.title + "=" + self.get_dictionary_value(app_set.title) + "\n\n"
                 else:
@@ -336,11 +346,14 @@ class NikGappsConfig:
             print("Invalid config path!")
 
     def upgrade_config(self):
+        config_objects = self.build_config_objects()
         standard_dict = self.get_config_dictionary(
-            self.get_nikgapps_config(config_objects=self.build_config_objects(), for_release=True, override=True))
+            self.get_nikgapps_config(config_objects=config_objects, for_release=True, override=True))
         for key in standard_dict.keys():
             if self.config_dict.get(key) is None:
                 self.config_dict[key] = standard_dict[key]
         self.config_dict["Version"] = self.config_version
+        config_objects = self.build_config_objects(self.config_dict)
         self.config_package_list = self.get_config_packages()
-        self.config_string = self.get_nikgapps_config(config_dict=self.config_dict, for_release=True)
+        self.config_string = self.get_nikgapps_config(config_dict=self.config_dict, config_objects=config_objects,
+                                                      for_release=True)
