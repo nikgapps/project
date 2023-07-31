170fdbcf04ad82130ce259385ccd6c950aa9d227
Nikhil Menghani <nikhil@menghani.com>
2023-07-31 11:56:33 -0400

    Log1 of how pexpect behaves

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
+import os
+
+from NikGapps.helper.Cmd import Cmd
 from NikGapps.helper.upload.CmdUpload import CmdUpload
 
-upload = CmdUpload('13', 'stable', True)
-print(upload.successful_connection)
-upload.create_directory_structure("A/B/C")
+# upload = CmdUpload('13', 'stable', True)
+# print(upload.successful_connection)
+# upload.create_directory_structure("A/B/C")
 
+import pexpect
+password = os.environ.get('SF_PWD')
+child = pexpect.spawn('sftp nikhilmenghani@frs.sourceforge.net')  # replace with your user and host
+i = child.expect(["Password", "yes/no", pexpect.TIMEOUT, pexpect.EOF], timeout=120)
+print(i)
+print(child.before)
+print(child)
+child.sendline(str(password))
+print(child.before)
+print(child)
