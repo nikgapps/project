e2867f2093371d85fa9ee96aa6cd651ea9c4606f
Nikhil Menghani <nikhil@menghani.com>
2023-10-04 18:55:03 -0400

    Mounting improvements from anykernel3 implementation by @osm0sis

--- NikGapps/helper/assets/mount.sh
+++ NikGapps/helper/assets/mount.sh
@@ -37,82 +37,63 @@ is_mounted_rw() {
 
 # Mount all the partitions
 mount_all() {
+  addToGeneralLog "----------------------------------------------------------------------------" "$mountLog"
   local byname mount slot system;
-  if ! is_mounted /cache; then
-    mount /cache 2>/dev/null && UMOUNT_CACHE=1
-  fi
-  if ! is_mounted /data; then
-    ui_print "- Mounting /data" "$mountLog"
-    $BB mount /data && UMOUNT_DATA=1
-  else
-    addToGeneralLog "- /data already mounted!" "$mountLog"
-  fi;
-
-  (for mount in /vendor /product /system_ext /persist; do
-    ui_print "- Mounting $mount" "$mountLog"
-    $BB mount -o ro -t auto $mount;
-  done) 2>/dev/null;
-
-  addToGeneralLog "----------------------------------------------------------------------------" "$mountLog"
-  ui_print "- Mounting $ANDROID_ROOT" "$mountLog"
-  addToGeneralLog "- Setting up mount point $ANDROID_ROOT" "$mountLog"
-  addToGeneralLog "- ANDROID_ROOT=$ANDROID_ROOT" "$mountLog"
-  setup_mountpoint "$ANDROID_ROOT"
-  if ! is_mounted "$ANDROID_ROOT"; then
-    mount -o ro -t auto "$ANDROID_ROOT" 2>/dev/null
-  fi
-  addToGeneralLog "----------------------------------------------------------------------------" "$mountLog"
+  addToGeneralLog "- Mounting..." "$mountLog"
   byname=bootdevice/by-name;
-  [ -d /dev/block/$byname ] || byname=$($BB find /dev/block/platform -type d -name by-name 2>/dev/null | $BB head -n1 | $BB cut -d/ -f4-);
-  [ -d /dev/block/mapper ] && byname=mapper && addToGeneralLog "- Device with dynamic partitions Found" "$mountLog";
+  [ -d /dev/block/$byname ] || byname=$(find /dev/block/platform -type d -name by-name 2>/dev/null | head -n1 | cut -d/ -f4-);
+  addToGeneralLog "- byname=$byname" "$mountLog";
+  [ -e /dev/block/$byname/super ] && [ -d /dev/block/mapper ] && byname=mapper && addToGeneralLog "- Device with dynamic partitions Found" "$mountLog";
   [ -e /dev/block/$byname/system ] || slot=$(find_slot);
-  [ -e /dev/block/$byname/system ] || slot=$(find_slot);
+  for mount in /cache /data /metadata /persist; do
+    if ! is_mounted $mount; then
+      mount $mount 2>/dev/null && addToGeneralLog "- $mount (fstab)" "$mountLog" >&2 && UMOUNTLIST="$UMOUNTLIST $mount";
+      if [ $? != 0 ] && [ -e /dev/block/$byname$mount ]; then
+        setup_mountpoint $mount;
+        mount -o rw -t auto /dev/block/$byname$mount $mount && addToGeneralLog "- $mount (direct)" "$mountLog" >&2 && UMOUNTLIST="$UMOUNTLIST $mount";
+      fi;
+    fi;
+  done;
+  ui_print "- Mounting $ANDROID_ROOT" "$mountLog"
+  setup_mountpoint $ANDROID_ROOT;
+  if ! is_mounted $ANDROID_ROOT; then
+    mount -o ro -t auto $ANDROID_ROOT 2>/dev/null && addToGeneralLog "- $ANDROID_ROOT (\$ANDROID_ROOT)" "$mountLog" >&2;
+  fi;
   case $ANDROID_ROOT in
     /system_root) setup_mountpoint /system;;
     /system)
       if ! is_mounted /system && ! is_mounted /system_root; then
         setup_mountpoint /system_root;
-        addToGeneralLog "- mounting /system_root partition as readwrite" "$mountLog"
-        $BB mount -o rw -t auto /system_root;
+        mount -o rw -t auto /system_root && addToGeneralLog "- /system_root (fstab)" "$mountLog" >&2;
       elif [ -f /system/system/build.prop ]; then
         setup_mountpoint /system_root;
-        addToGeneralLog "- Moving /system to /system_root" "$mountLog"
-        $BB mount --move /system /system_root;
+        mount --move /system /system_root && addToGeneralLog "- /system_root (moved)" "$mountLog" >&2;
       fi;
-      ret=$?
-      addToGeneralLog "- Command Execution Status: $ret" "$mountLog"
-      if [ $ret -ne 0 ]; then
-        addToGeneralLog "- Unmounting and Remounting /system as /system_root" "$mountLog"
-        ($BB umount /system;
-        $BB umount -l /system) 2>/dev/null
-        $BB mount -o ro -t auto /dev/block/$byname/system$slot /system_root;
-      else
-         addToGeneralLog "- $ret should be equals to 0" "$mountLog"
-      fi
+      if [ $? != 0 ]; then
+        (umount /system;
+        umount -l /system) 2>/dev/null;
+        mount -o rw -t auto /dev/block/$byname/system$slot /system_root && addToGeneralLog "- /system_root (direct)" "$mountLog" >&2;
+      fi;
     ;;
   esac;
   [ -f /system_root/system/build.prop ] && system=/system;
   for mount in /vendor /product /system_ext; do
-      if ! is_mounted $mount && [ -L /system$mount -o -L /system_root$system$mount ]; then
-        setup_mountpoint $mount;
-        $BB mount -o ro -t auto /dev/block/$byname$mount$slot $mount;
-      fi;
+    ui_print "- Mounting $mount" "$mountLog"
+    mount -o rw -t auto $mount 2>/dev/null && addToGeneralLog "- $mount (fstab)" "$mountLog" >&2;
+    if [ $? != 0 ] && [ -L /system$mount -o -L /system_root$system$mount ]; then
+      setup_mountpoint $mount;
+      mount -o rw -t auto /dev/block/$byname$mount$slot $mount && addToGeneralLog "- $mount (direct)" "$mountLog" >&2;
+    fi;
   done;
-  addToGeneralLog "----------------------------------------------------------------------------" "$mountLog"
-  addToGeneralLog "- Checking if /system_root is mounted.." "$mountLog"
-  addToGeneralLog "----------------------------------------------------------------------------" "$mountLog"
+
   if is_mounted /system_root; then
     mount_apex;
-    $BB mount -o bind /system_root$system /system;
+    mount -o bind /system_root$system /system && addToGeneralLog "- /system (bind)" "$mountLog" >&2;
   elif is_mounted /system; then
     addToGeneralLog "- /system is mounted" "$mountLog"
   else
     addToGeneralLog "- Could not mount /system" "$mountLog"
     abort "- Could not mount /system, try changing recovery!"
-  fi;
-  if ! is_mounted /persist && [ -e /dev/block/bootdevice/by-name/persist ]; then
-    setup_mountpoint /persist;
-    $BB mount -o ro -t auto /dev/block/bootdevice/by-name/persist /persist;
   fi;
   addToGeneralLog "----------------------------------------------------------------------------" "$mountLog"
   system=/system
@@ -123,7 +104,7 @@ mount_all() {
         blockdev --setrw /dev/block/mapper/$block$slot 2>/dev/null
       done
     done
-  addToGeneralLog "----------------------------------------------------------------------------" "$mountLog"
+    addToGeneralLog "----------------------------------------------------------------------------" "$mountLog"
   fi
   mount -o rw,remount -t auto /system || mount -o rw,remount -t auto /
   for partition in "vendor" "product" "system_ext"; do
