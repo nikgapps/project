9eb5abf1497afcf619d11993d1e9442ed18944ad
Nikhil Menghani <nikhil@menghani.com>
2024-05-15 14:44:50 -0400

    Remove Operation.py as it is not needed

--- NikGapps/main.py
+++ NikGapps/main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
-from NikGapps.build.Operation import Operation
+from NikGapps.build.Release import Release
 from NikGapps.helper.Args import Args
 from NikGapps.helper import Config
+from NikGapps.helper.Json import Json
+from NikGapps.helper.Statics import Statics
 from NikGapps.helper.SystemStat import SystemStat
 from NikGapps.helper.P import P
 from NikGapps.helper.T import T
 from NikGapps.helper.compression.Modes import Modes
 from NikGapps.helper.git.GitOperations import GitOperations
+from NikGapps.helper.upload.Upload import Upload
+from NikGapps.helper.web.Requests import Requests
 from NikGapps.helper.web.TelegramApi import TelegramApi
 
 
@@ -35,11 +39,26 @@ def main():
     print("Packages to build: " + str(package_list))
     print("---------------------------------------")
 
-    operation = Operation()
     telegram = TelegramApi(Config.TELEGRAM_BOT_TOKEN, Config.TELEGRAM_CHAT_ID)
-    operation.build(git_clone=args.enable_git_clone, sign_zip=args.sign, arch=args.arch,
-                    android_versions=android_versions, package_list=package_list, telegram=telegram,
-                    is_release=args.release)
+    for android_version in android_versions:
+        Json.write_dict_to_file(Requests.get_package_details(), Statics.get_package_details(android_version))
+        Statics.package_details = Json.read_dict_from_file(Statics.get_package_details(android_version))
+        upload = Upload(android_version=android_version, upload_files=Config.UPLOAD_FILES,
+                        release_type=Config.RELEASE_TYPE)
+        Config.TARGET_ANDROID_VERSION = android_version
+        # clone the apk repo if it doesn't exist
+        if args.enable_git_clone:
+            GitOperations.clone_apk_repo(android_version, args.arch, branch="main" if Config.RELEASE_TYPE.__eq__(
+                "stable") else "canary")
+            GitOperations.clone_overlay_repo(android_version)
+            if Config.USE_CACHED_APKS:
+                GitOperations.clone_apk_repo(android_version, branch="main", cached=True)
+        if Config.OVERRIDE_RELEASE:
+            Release.zip(package_list, android_version, args.arch, args.sign, Config.SEND_ZIP_DEVICE, Config.FRESH_BUILD, telegram,
+                        upload)
+        upload.close_connection()
+        if Config.RELEASE_TYPE:
+            GitOperations.mark_a_release(android_version, Config.RELEASE_TYPE)
     if args.release:
         if args.update_website:
             website_repo = GitOperations.get_website_repo_for_changelog()
--- NikGapps/test.py
+++ NikGapps/test.py
@@ -1,19 +1,22 @@
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
+# from cryptography.hazmat.primitives.asymmetric import rsa
+# from cryptography.hazmat.primitives import serialization
+#
+# # Generate a private key
+# private_key = rsa.generate_private_key(
+#     public_exponent=65537,
+#     key_size=2048,
+# )
+#
+# # Serialize the private key to PEM format
+# pem = private_key.private_bytes(
+#     encoding=serialization.Encoding.PEM,
+#     format=serialization.PrivateFormat.TraditionalOpenSSL,
+#     encryption_algorithm=serialization.NoEncryption()
+# )
+#
+# # Write the PEM file to disk
+# with open('private_key.pem', 'wb') as pem_file:
+#     pem_file.write(pem)
+from NikGapps.helper.web.Requests import Requests
 
-# Generate a private key
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-)
-
-# Serialize the private key to PEM format
-pem = private_key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.TraditionalOpenSSL,
-    encryption_algorithm=serialization.NoEncryption()
-)
-
-# Write the PEM file to disk
-with open('private_key.pem', 'wb') as pem_file:
-    pem_file.write(pem)
+print(Requests.get_package_details())
